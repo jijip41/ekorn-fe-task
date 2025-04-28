27167a45eca2882d45143177fa7410c02b39d7db
jihye <jiji.pak41@gmail.com>
2025-04-29 00:32:04 +0900

    Refactor: create StudentCard component

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,16 @@
 <script lang="ts" module>
+  import StudentCard from './lib/components/StudentCard.svelte';
   import { studentsData } from './lib/data';
   import { transformStudents, type Student } from './lib/utils/students';
 
   const students: Student[] = transformStudents(studentsData);
-  console.log({ students });
 </script>
 
 <main>
   <!-- The list of students -->
   <h1>Students</h1>
   {#each students as student}
-    <article>
-      <h2>{student.name}</h2>
-      <ul>
-        <li>Age: {student.age}</li>
-        <li>Average score: {student.averageScore}</li>
-        <li>Active: {student.activeLabel}</li>
-        <li>Passed: {student.passedLabel}</li>
-        <li>ID: {student.id}</li>
-      </ul>
-    </article>
+    <StudentCard {student} />
   {/each}
 </main>
 
---
+++ src/lib/components/StudentCard.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { Student } from '../utils/students';
+
+  type Props = {
+    student: Student;
+  };
+
+  let { student }: Props = $props();
+</script>
+
+<article>
+  <h2>{student.name}</h2>
+  <ul>
+    <li>Age: {student.age}</li>
+    <li>Average score: {student.averageScore}</li>
+    <li>Active: {student.activeLabel}</li>
+    <li>Passed: {student.passedLabel}</li>
+    <li>ID: {student.id}</li>
+  </ul>
+</article>
