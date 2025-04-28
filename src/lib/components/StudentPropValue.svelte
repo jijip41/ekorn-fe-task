e565296632a1bd28bed68e858fb0c39e3c2e690c
jihye <jiji.pak41@gmail.com>
2025-04-29 01:08:01 +0900

    Style StudentPropValue component

--- src/lib/components/StudentPropValue.svelte
+++ src/lib/components/StudentPropValue.svelte
@@ -7,7 +7,30 @@
   let { title, value }: Props = $props();
 </script>
 
-<p>
+<p class="container">
   <span class="title">{title}:</span>
   <span class="value">{value}</span>
 </p>
+
+<style>
+  .container {
+    font-size: var(--font-size-base);
+    font-family: var(--font-family-card);
+    line-height: 100%;
+    font-size: 20px;
+    height: 15px;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .title {
+    font-weight: 400;
+    color: var(--text-title-color);
+  }
+
+  .value {
+    font-weight: 700;
+    text-align: right;
+    color: var(--black);
+  }
+</style>
