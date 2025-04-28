851a3ac1fcdbb690d0b5aa5683a0be603f566d25
jihye <jiji.pak41@gmail.com>
2025-04-29 00:26:42 +0900

    Present students data

--- src/App.svelte
+++ src/App.svelte
@@ -8,8 +8,18 @@
 
 <main>
   <!-- The list of students -->
+  <h1>Students</h1>
   {#each students as student}
-    <p>{student.name}</p>
+    <article>
+      <h2>{student.name}</h2>
+      <ul>
+        <li>Age: {student.age}</li>
+        <li>Average score: {student.averageScore}</li>
+        <li>Active: {student.activeLabel}</li>
+        <li>Passed: {student.passedLabel}</li>
+        <li>ID: {student.id}</li>
+      </ul>
+    </article>
   {/each}
 </main>
 
