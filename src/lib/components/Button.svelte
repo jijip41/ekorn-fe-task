5e9d05d6b940724aeb46e914a8f79a9603b21d61
jihye <jiji.pak41@gmail.com>
2025-05-08 22:11:09 +0900

    Create Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  type Props = {
+    label: string;
+    onclick: () => void;
+  };
+  let { label, onclick }: Props = $props();
+</script>
+
+<button {onclick}>{label}</button>
+
+<style>
+  button {
+    cursor: pointer;
+    background-color: var(--white);
+    border-radius: 5px;
+    border: solid 2px var(--black);
+  }
+
+  button:hover {
+    box-shadow: 0px 2px 4px 0px var(--shadow-color);
+    font-weight: var(--font-weight-lg);
+  }
+</style>
